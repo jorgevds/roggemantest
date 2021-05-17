c526b0aa9df032c4edf6d825a04f7a062f7c7a01
jorgevds <jorgevandesompel@gmail.com>
2021-05-17 22:54:18 +0200

    first draft of full text

---
+++ script.js
@@ -0,0 +1,7 @@
+function tooltip() {
+  console.log("clicked");
+  document
+    .getElementById("tooltiptext-two")
+    .classList.toggle("tooltiptext-two-visible");
+  document.getElementById("tooltip").classList.toggle("tooltip");
+}
